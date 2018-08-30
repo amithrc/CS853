1d09c87a62d72f0f815eb1f3c52328a3f986283d
MSI\amith <amithrc07@gmail.com>
2018-08-30 16:29:30 -0400

    Initial commit

---
+++ src/main/java/LuceneIndex/LuceneMain.java
@@ -0,0 +1,10 @@
+package main.java.LuceneIndex;
+import org.apache.lucene.queryparser.classic.QueryParser;
+
+public class LuceneMain
+{
+    public static void main(String[] args)
+    {
+        System.out.println("welcome to Lucene");
+    }
+}
---
+++ src/main/java/LuceneSearch/LuceneSeacrher.java
@@ -0,0 +1,5 @@
+package main.java.LuceneSearch;
+public class LuceneSeacrher
+{
+
+}
