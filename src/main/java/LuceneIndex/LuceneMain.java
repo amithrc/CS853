d3c816176ea1df1e127533f85ac8df22a8fe5321
MSI\amith <amithrc07@gmail.com>
2018-09-13 20:26:13 -0400

    Main function

--- src/main/java/LuceneIndex/LuceneMain.java
+++ src/main/java/LuceneIndex/LuceneMain.java
@@ -31,37 +31,37 @@ public class LuceneMain
 	public static void main(String[] args) throws IOException
 	{
 		String dest;
-		System.out.println("Please pass the file to be indexed");
+		//System.out.println("Please pass the file to be indexed");
 		if( args.length < 1 )
 		{
 			usage();
 		}
 		else
 		{
-
+		/*
 				dest = System.getProperty("user.dir")+System.getProperty("file.separator")+"indexed_file";
 				String[] mode_input = new String[] {"paragraphs", args[0]};
-				
+
 				//Sets the file directory that the corpus is coming from
 				LuceneConstants.setIndexFileName(args[0]);
 				LuceneConstants.setDirectoryName(dest);
-				
+
 				//Create the new lucene Index
 				LuceneIndexer l = new LuceneIndexer();
 				l.setMode(mode_input);
 				l.getIndexWriter(dest);
 				l.closeIndexWriter();
-				
+
 				System.out.println();
                 System.out.println("Starting the basic search...");
-                
+
                 //Run basic search
                 LuceneSearcher basicSearcher = new LuceneSearcher(false);
                 basicSearcher.getRankingDocuments();
-                
+
                 System.out.println();
 				System.out.println("Starting the Custom search...");
-				
+
 				//Run advanced search
                 LuceneSearcher customSearcher = new LuceneSearcher(true);
                 customSearcher.getRankingDocuments();
@@ -70,12 +70,23 @@ public class LuceneMain
 				Map<String,String> p =LuceneUtil.readQrel(file);
 
 
+*/
+
+			String path="C:\\Users\\amith\\Downloads\\test200.v2.0.tar\\test200.v2.0\\test200\\test200-train\\train.pages.cbor-article.qrels";
+
+			Map<String,Map<String,Integer>> qrel = LuceneUtil.createQrelMap(path);
+			System.out.println(LuceneUtil.relevancy_count(qrel,"enwiki:Zang-fu"));
+
+
+
+
+
 
 
 
 		}
 
-		}
+	}
 
 }
 
