02e91eebc18802c4894b01868872091ab11f5478
MSI\amith <amithrc07@gmail.com>
2018-09-13 20:25:20 -0400

    Adding the MAP function

--- src/main/java/util/LuceneUtil.java
+++ src/main/java/util/LuceneUtil.java
@@ -2,13 +2,9 @@ package main.java.util;
 import edu.unh.cs.treccar_v2.read_data.DeserializeData;
 import edu.unh.cs.treccar_v2.Data;
 
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.HashMap;
-import java.util.List;
+
 import java.util.Map;
 
 
@@ -26,11 +22,109 @@ public class LuceneUtil
 
         }
         for (Data.Page page : DeserializeData.iterableAnnotations(qrelStream)) {
-            //System.out.println(page.getPageId());
-            //System.out.println(page.getPageName());
             data.put(page.getPageId(),page.getPageName());
         }
         return data;
     }
+
+    public static int relevancy_count(Map<String,Map<String,Integer>> m , String query_id)
+    {
+
+        if(m.containsKey(query_id))
+        {
+            Map<String,Integer> temp = m.get(query_id);
+            return temp.size();
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+
+    static public Map<String,Map<String,Integer>> createQrelMap(String filename)
+    {
+        Map<String,Map<String,Integer>> mp = new HashMap<String, Map<String, Integer>>();
+
+        File fp= new File(filename);
+        FileReader fr;
+        BufferedReader br = null;
+
+
+        try {
+            fr = new FileReader(fp);
+            br = new BufferedReader(fr);
+
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        while(true)
+        {
+            try
+            {
+                String line = br.readLine();
+
+                if(line == null )
+                {
+                    break;
+                }
+
+                String[] words =  line.split(" ");
+                String outKey = words[0];
+
+                if(mp.containsKey(outKey))
+                {
+                    Map<String, Integer> extract = mp.get(outKey);
+                    String inner_key = words[2];
+                    Integer is_relevant= new Integer(words[3]);
+                    extract.put(inner_key,is_relevant);
+                }
+                else
+                {
+
+                    String inner_key = words[2];
+                    Integer is_relevant= new Integer(words[3]);
+                    Map<String,Integer> temp = new HashMap<String, Integer>();
+                    temp.put(inner_key,is_relevant);
+                    mp.put(outKey,temp);
+                }
+            }
+            catch (NullPointerException n)
+            {
+                System.out.println(n.getMessage());
+            }
+            catch(IOException e)
+            {
+                System.out.println(e.getMessage());
+            }
+
+        }
+
+        /*for (Map.Entry<String, Map<String,Integer>> item : mp.entrySet())
+        {
+            String outkey = item.getKey();
+            Map<String,Integer> temp= item.getValue();
+
+            System.out.println(outkey);
+            for(Map.Entry<String,Integer> inside: temp.entrySet())
+            {
+                System.out.println(inside.getKey()+"  "+ inside.getValue());
+            }
+            System.out.println("-----------------------------------------------");
+
+        }
+
+        System.out.println(relevancy_count(mp,"enwiki:Activity%20theory"));
+
+        */
+
+
+        return mp;
+
+    }
+
 
 }
