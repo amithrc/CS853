0843f9eb3f5cc59e31d2eeb62092a4dc25662de5
MSI\amith <amithrc07@gmail.com>
2018-09-29 02:02:53 -0400

    Removed not used functions

--- src/main/java/util/LuceneUtil.java
+++ src/main/java/util/LuceneUtil.java
@@ -7,11 +7,9 @@ import java.util.*;
 import java.io.File;
 
 
-public class LuceneUtil
-{
-    static public Map<String, String> readOutline(String filename)
-    {
-        Map<String,String> data = new LinkedHashMap<String,String>();
+public class LuceneUtil {
+    static public Map<String, String> readOutline(String filename) {
+        Map<String, String> data = new LinkedHashMap<String, String>();
 
         FileInputStream qrelStream = null;
         try {
@@ -21,31 +19,26 @@ public class LuceneUtil
 
         }
         for (Data.Page page : DeserializeData.iterableAnnotations(qrelStream)) {
-            data.put(page.getPageId(),page.getPageName());
+            data.put(page.getPageId(), page.getPageName());
         }
         return data;
     }
 
-    public static int relevancy_count(Map<String,Map<String,Integer>> m, String query_id)
-    {
-        if(m.containsKey(query_id))
-        {
-            Map<String,Integer> temp = m.get(query_id);
+    public static int relevancy_count(Map<String, Map<String, Integer>> m, String query_id) {
+        if (m.containsKey(query_id)) {
+            Map<String, Integer> temp = m.get(query_id);
             return temp.size();
-        }
-        else
-        {
+        } else {
             return 0;
         }
 
     }
 
-    /*O*/
-    static public Map<String,Map<String,Integer>> createQrelMap(String filename)
-    {
-        Map<String,Map<String,Integer>> mp = new LinkedHashMap<String, Map<String, Integer>>();
 
-        File fp= new File(filename);
+    static public Map<String, Map<String, Integer>> createQrelMap(String filename) {
+        Map<String, Map<String, Integer>> mp = new LinkedHashMap<String, Map<String, Integer>>();
+
+        File fp = new File(filename);
         FileReader fr;
         BufferedReader br = null;
 
@@ -54,49 +47,37 @@ public class LuceneUtil
             fr = new FileReader(fp);
             br = new BufferedReader(fr);
 
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-        while(true)
-        {
-            try
-            {
+        while (true) {
+            try {
                 String line = br.readLine();
 
-                if(line == null )
-                {
+                if (line == null) {
                     break;
                 }
 
-                String[] words =  line.split(" ");
+                String[] words = line.split(" ");
                 String outKey = words[0];
 
-                if(mp.containsKey(outKey))
-                {
+                if (mp.containsKey(outKey)) {
                     Map<String, Integer> extract = mp.get(outKey);
                     String inner_key = words[2];
-                    Integer is_relevant= new Integer(words[3]);
-                    extract.put(inner_key,is_relevant);
-                }
-                else
-                {
+                    Integer is_relevant = new Integer(words[3]);
+                    extract.put(inner_key, is_relevant);
+                } else {
 
                     String inner_key = words[2];
-                    Integer is_relevant= new Integer(words[3]);
-                    Map<String,Integer> temp = new LinkedHashMap<String, Integer>();
-                    temp.put(inner_key,is_relevant);
-                    mp.put(outKey,temp);
+                    Integer is_relevant = new Integer(words[3]);
+                    Map<String, Integer> temp = new LinkedHashMap<String, Integer>();
+                    temp.put(inner_key, is_relevant);
+                    mp.put(outKey, temp);
                 }
-            }
-            catch (NullPointerException n)
-            {
+            } catch (NullPointerException n) {
                 System.out.println(n.getMessage());
-            }
-            catch(IOException e)
-            {
+            } catch (IOException e) {
                 System.out.println(e.getMessage());
             }
 
@@ -105,14 +86,11 @@ public class LuceneUtil
 
     }
 
-    static public void DisplayMap(Map<String,Map<String,Integer>> q)
-    {
-        for (Map.Entry<String, Map<String,Integer>> Query : q.entrySet())
-        {
+    static public void DisplayMap(Map<String, Map<String, Integer>> q) {
+        for (Map.Entry<String, Map<String, Integer>> Query : q.entrySet()) {
 
-            for (Map.Entry<String,Integer> p: Query.getValue().entrySet())
-            {
-                System.out.println(Query.getKey()+","+p.getKey());
+            for (Map.Entry<String, Integer> p : Query.getValue().entrySet()) {
+                System.out.println(Query.getKey() + "," + p.getKey());
 
             }
             System.out.println("----------------------------------------------------------------------------------------------------");
@@ -121,24 +99,4 @@ public class LuceneUtil
 
     }
 
-//    public static  boolean validateInput(String d, String q)
-//    {
-//        char[] doc = d.toCharArray();
-//        char [] que =q.toCharArray();
-//        //List<String> known_var = new ArrayList<String>();
-//
-//        boolean doc1 = Arrays.asList(LuceneConstants.known_variants).containsAll(Arrays.asList(doc));
-//        boolean que1 = Arrays.asList(LuceneConstants.known_variants).containsAll(Arrays.asList(que));
-//
-//        System.out.println(doc1);
-//        System.out.println(que1);
-//
-//        System.out.println( Arrays.asList(LuceneConstants.known_variants).get(0));
-//
-//        System.out.println(doc);
-//
-//
-//        return true;
-//    }
 }
-
